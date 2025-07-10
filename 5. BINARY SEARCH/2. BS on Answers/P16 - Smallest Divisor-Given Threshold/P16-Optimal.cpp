98f9c91ace603218dcb4df0a8fa309a447b7239a
Utkarsh Jain <jain.utkarsh.5605@gmail.com>
2025-07-10 23:26:41 +0530

    Add files via upload

---
+++ 5. BINARY SEARCH/2. BS on Answers/P16 - Smallest Divisor-Given Threshold/P16-Optimal.cpp
@@ -0,0 +1,51 @@
+// Small Divisor - Given Threshold. (Optimal)  -->  TC:O(n*log(max_ele(arr)))   SC:O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int max_ele(vector<int> &arr)
+{
+    int maxi = INT_MIN;
+    for (auto i : arr)
+    {
+        maxi = max(maxi, i);
+    }
+    return maxi;
+}
+
+int check(vector<int> &arr, int d)
+{
+    int sum = 0;
+    for (auto i : arr)
+    {
+        sum += ceil((double)i / d);
+    }
+    return sum;
+}
+
+int fnc(vector<int> &arr, int t)
+{
+    int n = arr.size();
+
+    // n is the min sum.
+    if (n > t)
+        return -1;
+
+    int low = 1, high = max_ele(arr);
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (check(arr, mid) <= t)
+            high = mid - 1;
+        else
+            low = mid + 1;
+    }
+    return low;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 5, 9};
+    int thr = 6;
+    cout << fnc(arr, thr);
+}
