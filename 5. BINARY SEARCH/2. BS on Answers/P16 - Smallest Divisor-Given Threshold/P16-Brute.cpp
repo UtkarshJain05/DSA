921946c75c1105dd98bc5bd45502a905195ac345
Utkarsh Jain <jain.utkarsh.5605@gmail.com>
2025-07-10 23:21:57 +0530

    Add files via upload

---
+++ 5. BINARY SEARCH/2. BS on Answers/P16 - Smallest Divisor-Given Threshold/P16-Brute.cpp
@@ -0,0 +1,41 @@
+// Small Divisor - Given Threshold. (Burte)  -->  TC:O(n*max_ele(arr))   SC:O(1)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int max_ele(vector<int> &arr)
+{
+    int maxi = INT_MIN;
+    for (auto i : arr)
+    {
+        maxi = max(maxi, i);
+    }
+    return maxi;
+}
+
+int check(vector<int> &arr, int d)
+{
+    int sum = 0;
+    for (auto i : arr)
+    {
+        sum += ceil((double)i / d);
+    }
+    return sum;
+}
+
+int fnc(vector<int> &arr, int t)
+{
+    for (int i = 1; i <= max_ele(arr); i++)
+    {
+        if (check(arr, i) <= t)
+            return i;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 5, 9};
+    int thr = 6;
+    cout << fnc(arr, thr);
+}
